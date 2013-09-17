3ec0139f3067e29b7f4246b0f6c261966dee50ff
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-17 14:30:04 +1000

    edited start variable
    it should be burnin+1, not burnin

--- cumulative.freq.R
+++ cumulative.freq.R
@@ -1,23 +1,24 @@
 cumulative.freq <- function(x, burnin=0, window, gens.per.tree = 1, ...){ 
     #Specify burnin in TREES, not GENERATIONS
 
+    start <- burnin + 1
+
     # figure out how many segments to split the list of trees into
-    start <- burnin
+
     n.windows <- as.integer((length(x) - burnin)/window)
 
     print("Populating table...")
 
     #Getting all clades from chain into table
-    allnames <- clade.freq(x, start = burnin, end  = length(x))$cladefreqs[,1] 
+    allnames <- clade.freq(x, start = start, end  = length(x))$cladefreqs[,1] 
 
-    
     clade.table <- as.data.frame(matrix(0, ncol=0, nrow = length(allnames)))
     
     clade.table$cladenames <- allnames
 
     for(i in 1:n.windows){
         print(paste("window", i, "of", n.windows))
-        thiswindow.table <- clade.freq(x, start = burnin, end  = burnin + (i) * window)
+        thiswindow.table <- clade.freq(x, start = start, end  = burnin + (i) * window)
         clade.table <- merge(clade.table, thiswindow.table$cladefreqs, by = "cladenames", all = TRUE)
         colnames(clade.table)[i+1] <- ((i-1) * window * gens.per.tree) + (burnin * gens.per.tree)
     }
