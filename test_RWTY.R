3c831edc23269f33cc087bcfd5f851506e4831fb
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-17 22:25:18 +1000

    moved test data to testdata folder
    and updated the test_RWTY.R accordingly

--- test_RWTY.R
+++ test_RWTY.R
@@ -3,14 +3,23 @@ setwd("~/Dropbox/R Projects/RWTY")
 
 source("RWTY.R")
 
-test1 <- read.nexus(file="PCFW.nex.run1.t")
-test2 <- read.nexus(file="PCFW.nex.run2.t")
-test3 <- read.nexus(file="PCFW.nex.run1.t")
-test4 <- read.nexus(file="PCFW.nex.run2.t")
+test1 <- read.nexus(file="./testdata/fairywrens/PCFW.nex.run1.t")
+test2 <- read.nexus(file="./testdata/fairywrens/PCFW.nex.run2.t")
+test3 <- read.nexus(file="./testdata/fairywrens/PCFW.nex.run1.t")
+test4 <- read.nexus(file="./testdata/fairywrens/PCFW.nex.run2.t")
 cumtest <- cumulative.freq(test1, burnin=100, window=100, gens.per.tree=1000)
 slidetest <- slide.freq(test1, burnin=100, window=100, gens.per.tree=1000)
 comp2test <- compare.two(test1, test2, burnin=100)
 compntest <- compare.n(x=list(test1, test2, test3, test4), setnames = c("test1", "test2", "test3", "test4"), burnin=100)
 # Change in SD along chain for cumulative?
 # Same for n chains?
-# For compare.n, use melt and plotmatrix
+
+
+test.hox1 <- read.nexus(file="./testdata/hoxgenes/Dataset2_con.nex.run1.t")
+test.hox2 <- read.nexus(file="./testdata/hoxgenes/Dataset2_con.nex.run2.t")
+cumtest <- cumulative.freq(test1, burnin=100, window=100, gens.per.tree=1000)
+slidetest <- slide.freq(test1, burnin=100, window=100, gens.per.tree=1000)
+plot.cladeprobs(cumtest, 20)
+plot.cladeprobs(slidetest, 20)
+plot.cladevar(cumtest, 20)
+plot.cladevar(slidetest, 20)
