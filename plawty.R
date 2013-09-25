3036efda84c0ec7bb0f0e74987eb56a2ac218e0e
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-25 17:26:53 +1000

    add catch for treespace plots without likelihoods
    so now we automatically deal with different situations, and produce
    nice plots either way.

--- plawty.R
+++ plawty.R
@@ -89,14 +89,21 @@ plot.mds.treespace <- function(points, dim){
     # This fundtion will take an mds.treespace object and produce plots 
     # of chains in treespace
     
-    p <- ggplot(data=points, aes(x=x,y=y,fill=LnL)) + 
-          geom_path(alpha=0.2, linetype='dashed') + 
-          geom_point(shape=21, size=7, alpha=0.85, colour='white') + 
-          scale_fill_gradient() +
-          geom_point(data=points, aes(x=x, y=y, colour=mcmc.sample), size=3)  + 
-          scale_colour_gradient(low='red', high='white')
+    if(!is.null(points$LnL)){
+        p <- ggplot(data=points, aes(x=x,y=y,fill=LnL)) + 
+              geom_path(alpha=0.2, linetype='dashed') + 
+              geom_point(shape=21, size=7, alpha=0.85, colour='white') + 
+              scale_fill_gradient() +
+              geom_point(data=points, aes(x=x, y=y, colour=mcmc.sample), size=3)  + 
+              scale_colour_gradient(low='red', high='white')
+    }
+    else{
+        p <- ggplot(data=points, aes(x=x,y=y)) + 
+              geom_path(alpha=0.2, linetype='dashed') + 
+              geom_point(data=points, aes(x=x, y=y, colour=mcmc.sample), size=3)  + 
+              scale_colour_gradient(low='red', high='white')
+    }
     p
-
 }
 
 
