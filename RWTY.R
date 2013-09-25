9aff02ac8092784480a20c675a1c956bdf2be6e8
RobertLanfear <rob.lanfear@gmail.com>
2013-09-25 16:09:56 +1000

    updated dependencies
    so that we can load liam revelles multiRF function from the non-CRAN
    phytools.
    
    #NB there will also be a similar function in phangorn some time soon.

--- RWTY.R
+++ RWTY.R
@@ -1,4 +1,5 @@
-library(ape)
+library(ape) #note that this needs to be >=3.0-10
+library(phytools) # note that the right version for this is not yet on cran, but here: http://www.phytools.org/nonstatic/phytools_0.3-53.tar.gz
 library(ggplot2)
 library(reshape2)
 library(phangorn)
