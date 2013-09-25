b9b856ef4ef0ef0afa119e86983d867ba86fcda1
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-25 20:03:54 +1000

    update tree.dist.matrix.R
    I still can't get Liam's function to run

--- tree.dist.matrix.R
+++ tree.dist.matrix.R
@@ -13,7 +13,7 @@ tree.dist.matrix <- function(trees, treenames=names(trees)){
         for(j in (i+1):N){
             RFd <- RF.dist(trees[[i]],trees[[j]])
             if(RFd==0) RFd = 0.000000001
-            RF[i,j]<-RF[j,i]<-RF.dist(trees[[i]],trees[[j]])
+            RF[i,j]<-RF[j,i]<-RFd
         }
     }
 
@@ -33,10 +33,10 @@ multiRF<-function(trees){
     if(class(trees)!="multiPhylo") stop("trees should be an object of class \"multiPhylo\"")
     N<-length(trees)
     RF<-matrix(0,N,N)
-    #if(any(sapply(unclass(trees),is.rooted))){
-    #    cat("Some trees are rooted. Unrooting all trees.\n")
-    #    trees<-lapply(unclass(trees),unroot)
-    #}
+    if(any(sapply(unclass(trees),is.rooted))){
+        cat("Some trees are rooted. Unrooting all trees.\n")
+        trees<-lapply(unclass(trees),unroot)
+    }
     foo<-function(pp) lapply(pp,function(x,pp) sort(attr(pp,"labels")[x]),pp=pp)
     xx<-lapply(unclass(trees),function(x) foo(prop.part(x)))
     for(i in 1:(N-1)) for(j in (i+1):N) RF[i,j]<-RF[j,i]<-2*sum(!xx[[i]]%in%xx[[j]])
