f68590e4edcc379ee86f215f5679297d09da43f5
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-25 16:36:11 +1000

    revert changes to dependencies
    because for now I've copied Liam's function in RWTY

--- RWTY.R
+++ RWTY.R
@@ -1,5 +1,4 @@
-library(ape) #note that this needs to be >=3.0-10
-library(phytools) # note that the right version for this is not yet on cran, but here: http://www.phytools.org/nonstatic/phytools_0.3-53.tar.gz
+library(ape)
 library(ggplot2)
 library(reshape2)
 library(phangorn)
