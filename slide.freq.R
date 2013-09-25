256cb2ea456f1ab84dde0c1f5d4c718dabee67f1
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-25 20:03:26 +1000

    fixed burnin in slide.freq.R
    and adde dsome user output

--- slide.freq.R
+++ slide.freq.R
@@ -1,8 +1,8 @@
 slide.freq <- function(tree.list, burnin=0, window.size, gens.per.tree = 1, ...){ 
     #Specify burnin in TREES, not GENERATIONS
-
-    start <- burnin + 1
-    n.windows <- as.integer((length(tree.list) - start)/window.size)
+    tree.list <- tree.list[burnin+1:length(tree.list)]
+    N <- length(tree.list)
+    n.windows <- as.integer(N/window.size)
 
     # first we slice up our tree list into smaller lists
     tree.index <- seq_along(tree.list)
@@ -12,14 +12,16 @@ slide.freq <- function(tree.list, burnin=0, window.size, gens.per.tree = 1, ...)
     clade.freq.list <- lapply(tree.windows, clade.freq, start=1, end=window.size)
 
     # and rename the list to the first tree of each window
-    names(clade.freq.list) = prettyNum(seq(1:length(clade.freq.list))*window.size*gens.per.tree, sci=TRUE)    
+    names(clade.freq.list) = prettyNum(seq(1:length(clade.freq.list))*window.size*gens.per.tree, sci=TRUE)
 
     # this is the table of frequencies in each window
+    print("Working on window 1")
     slide.freq.table <- clade.freq.list[[1]]
     colnames(slide.freq.table)[-1] <- names(clade.freq.list)[1]
 
     # now add in the rest
     for(i in 2:length(clade.freq.list)){
+        print(paste("Working on window", i))
         # add data to slide.freq table
         slide.freq.table <- merge(slide.freq.table, clade.freq.list[[i]], by="cladenames", all=TRUE)
         colnames(slide.freq.table)[which(colnames(slide.freq.table)=="cladefreqs")] <- names(clade.freq.list)[i]
