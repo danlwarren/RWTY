37a65501b1e48baef4a2d58fc96d6394047e37e7
Dan Warren <dan.l.warren@gmail.com>
2013-09-26 09:54:52 +1000

    Fix compare functions
    Compare functions were set up to use an old version of clade.freq, and
    were barfing.  It's fixed now.

--- compare.n.R
+++ compare.n.R
@@ -1,13 +1,13 @@
 compare.n <- function(x, setnames=NA, burnin){ # In this case x is a list of treefiles
   print("Populating table...")
   print(paste("Working on set", 1))
-  clade.table <- clade.freq(x[[1]], start=burnin, end=length(x[[1]]))$cladefreqs
+  clade.table <- clade.freq(x[[1]], start=burnin, end=length(x[[1]]))
   if(is.na(setnames[1])){colnames(clade.table)[2] <- paste("set", 1, sep=".")}
   else{colnames(clade.table)[2] <- setnames[1]}
   
   for(i in 2:length(x)){
     print(paste("Working on set", i))
-    thistable <- clade.freq(x[[i]], start = burnin, end  = length(x[[i]]))$cladefreqs 
+    thistable <- clade.freq(x[[i]], start = burnin, end  = length(x[[i]]))
     clade.table <- merge(clade.table, thistable, by = "cladenames", all = TRUE) 
     if(is.na(setnames[i])){colnames(clade.table)[i+1] <- paste("set", i, sep=".")}
     else{colnames(clade.table)[i+1] <- setnames[i]}
--- compare.two.R
+++ compare.two.R
@@ -1,18 +1,25 @@
 compare.two <- function(x, y, burnin){
+    
     start <- burnin + 1
+    
     print("Populating table...")
-
-    table1 <- clade.freq(x, start = start, end  = length(x))$cladefreqs 
-    table2 <- clade.freq(y, start = start, end  = length(y))$cladefreqs 
+    
+    table1 <- clade.freq(x, start = start, end  = length(x))
+    table2 <- clade.freq(y, start = start, end  = length(y))
+    
     clade.table <- merge(table1, table2, by = "cladenames", all = TRUE) 
     clade.table[is.na(clade.table)] <- 0
+    
     colnames(clade.table)[2] <- deparse(substitute(x))
     colnames(clade.table)[3] <- deparse(substitute(y))
+    
     thissd <- apply(clade.table[,2:length(clade.table[1,])], 1, sd)
     thismean <- apply(clade.table[,2:length(clade.table[1,])], 1, mean) 
     clade.table$sd <- thissd
     clade.table$mean <- thismean
+    
     clade.table <- clade.table[order(clade.table$sd, decreasing=TRUE),]
+    
     thisplot <- ggplot(data=clade.table[,2:3], aes(x=test1, y=test2)) + 
                 theme_bw() +
                 geom_point(shape = 19) + 
