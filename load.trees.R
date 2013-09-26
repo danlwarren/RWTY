7bf8cdf181a6deb0790f40d3339987c76afd6165
Dan Warren <dan.l.warren@gmail.com>
2013-09-26 13:09:33 +1000

    Fixed bug with lapply causing tip label to be lost
    replaced lapply with a loop, everything seems to work now

--- load.trees.R
+++ load.trees.R
@@ -1,19 +1,24 @@
 #custom functions to load tree lists so that we can do basic processing on the way in
 
 #sample that works
-load.trees <- function(x){
+load.trees <- function(file){
     
     # Read in trees
-    treelist <- read.nexus(file=x)
+    treelist <- read.nexus(file=file)
     
-    # Unroot all trees
-    treelist <- lapply(treelist, unroot)
+    # Unroot all trees.  Can't use lapply because it was
+    # deleting tip labels.
+    for(i in 1:length(treelist)){
+        treelist[[i]] <- unroot(treelist[[i]])
+    }
     
     # Reset class
     class(treelist) <- "multiPhylo"
     
+    ptable <- NA
+    
     # Check for .p file, read it in if it exists
-    pfile <- sub(".t$", ".p", x, perl=TRUE)
+    pfile <- sub(".t$", ".p", file, perl=TRUE)
     if(file.exists(pfile)){
         print(paste("Reading p values from", pfile))
         ptable <- read.table(pfile, skip=1, header=TRUE)
@@ -22,4 +27,6 @@ load.trees <- function(x){
     output <- list("trees" = treelist, "ptable" = ptable)
     
     class(output) <- "rwty.trees"
+    
+    output
 }
