9a200ebfb63d41ea4f96f9da6d775c0eebc0a1e3
Dan Warren <dan.l.warren@gmail.com>
2013-09-26 11:49:21 +1000

    First draft of load.trees.R
    New function that does the following
    
    Take a tree file name, load file into memory
    Unroot trees and reset to class multiPhylo (lapply deletes class info)
    Check for a .p file for the chain and load it if it exists
    Return an object containing the trees and p table, if it exists.

--- load.trees.R
+++ load.trees.R
@@ -1,1 +1,25 @@
 #custom functions to load tree lists so that we can do basic processing on the way in
+
+#sample that works
+load.trees <- function(x){
+    
+    # Read in trees
+    treelist <- read.nexus(file=x)
+    
+    # Unroot all trees
+    treelist <- lapply(treelist, unroot)
+    
+    # Reset class
+    class(treelist) <- "multiPhylo"
+    
+    # Check for .p file, read it in if it exists
+    pfile <- sub(".t$", ".p", x, perl=TRUE)
+    if(file.exists(pfile)){
+        print(paste("Reading p values from", pfile))
+        ptable <- read.table(pfile, skip=1, header=TRUE)
+    }
+    
+    output <- list("trees" = treelist, "ptable" = ptable)
+    
+    class(output) <- "rwty.trees"
+}
