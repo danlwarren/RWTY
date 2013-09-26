0ad9bc57dd0365643815de57daa9545024724ec1
Dan Warren <dan.l.warren@gmail.com>
2013-09-26 11:46:19 +1000

    Remove deprecated functions, add new
    Remove comparetwo, add parse.clades and load.trees

--- RWTY.R
+++ RWTY.R
@@ -7,11 +7,12 @@ source("clade.freq.R")
 source("cumulative.freq.R")
 source("slide.freq.R")
 source("plawty.R")
-source("compare.two.R")
 source("compare.n.R")
 source("tree.ess.R")
 source("tree.dist.matrix.R")
 source("mds.treespace.R")
 source("continuous.ess.R")
+source("parse.clades.R")
+source("load.trees.R")
 
 # test
