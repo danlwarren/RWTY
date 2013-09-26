7839055bc05de5c3a6f50e149f8e96c422daa100
Dan Warren <dan.l.warren@gmail.com>
2013-09-26 19:57:12 +1000

    Disabled debugging code
    Just turning off the tracking code to make analyse.rwty less messy
    during execution.  Again, we might want to hook this to a "verbose"
    option eventually.

--- tree.dist.matrix.R
+++ tree.dist.matrix.R
@@ -9,7 +9,7 @@ tree.dist.matrix <- function(trees, treenames=names(trees)){
     RF <- matrix(0, N, N)
         
     for(i in 1:(N-1)){
-        print(paste("Tree", i, "of", N))        
+        #print(paste("Tree", i, "of", N))        
         for(j in (i+1):N){
             RFd <- RF.dist(trees[[i]],trees[[j]])
             if(RFd==0) RFd = 0.000000001
