4181a6a97b7e2f646b1afe17a66ad59ba22383fe
Rob Lanfear <rob.lanfear@gmail.com>
2013-09-27 12:25:09 +1000

    better tree.ess plots

--- plawty.R
+++ plawty.R
@@ -74,13 +74,14 @@ abs.diffs <- function(x){
 }
 
 
-plot.tree.ess <- function(tree.ess.table, reference.average){
+plot.tree.ess <- function(tree.ess.object){
 
+    dat <- tree.ess.object$tree.distances
 
-    t <- ggplot(data=tree.ess.table, aes(x=gapsize, y=average.distance)) + 
-         geom_hline(yintercept=reference.average, linetype='dashed') +
-         geom_point(size = 2, aes(color=significant.difference)) + 
-         geom_smooth(size=0, method="loess") +
+    t <- ggplot(data=dat, aes(x=gapsize, y=average.distance)) + 
+         geom_hline(yintercept=tree.ess.object$reference.distance, linetype='dashed') +
+         geom_hline(yintercept=tree.ess.object$reference.distance.lowerCI, linetype='dashed', colour="gray") +
+         geom_point(size = 2, aes(color=lower)) + 
          theme(legend.position="none")
 
 
