14024129a379a2aec5892b115ac6174a9df8e068
RobertLanfear <rob.lanfear@gmail.com>
2013-11-06 10:28:30 +1100

    update sequential.distances.c.R
    added continuous distance function
    
    also commented out the bit that equalises the number of samples in each
    case. It's great for speeding things up, but I suspect that accuracy
    goes down...

--- rwty/R/get.sequential.distances.c.R
+++ rwty/R/get.sequential.distances.c.R
@@ -24,14 +24,17 @@ get.sequential.distances.c <- function(tree.list, thinning = 1, N=100){
     # first we cut off any trailing trees from the input list
     if(length(keep)%%2 != 0) keep <- keep[1:(length(keep)-1)]
     
-    # we only want to look at 100 samples, for efficiency
-    odds <- seq(from=1, to=length(keep), by=2) #indices of the tree1 trees in keep
-    if((length(odds))>N){
-        odds <- sample(odds[1:(length(odds))], N, replace=FALSE)
-        evens <- odds + 1 # indices of the tree2 trees in keep
-        indices <- sort(c(odds, evens))
-        keep <- keep[indices]
-    }
+    # now we get the indices of the odd elements of keep
+	# we will use these to make a pairwise list of sequential samples
+    odds <- seq(from=1, to=length(keep), by=2)
+    
+    # we only look at N samples, allows for variation in effciency    
+#    if((length(odds))>N){
+#        odds <- sample(odds[1:(length(odds))], N, replace=FALSE)
+#        evens <- odds + 1 # indices of the tree2 trees in keep
+#        indices <- sort(c(odds, evens))
+#        keep <- keep[indices]
+#    }
     
     tree.list <- tree.list[keep]
     tree.index <- seq_along(tree.list)
@@ -40,7 +43,15 @@ get.sequential.distances.c <- function(tree.list, thinning = 1, N=100){
     # e.g. c(a, b, c, d, e) -> c(a,b), c(c,d)
     tree.pairs <- split(tree.list, ceiling(tree.index/2))
     
+    print(paste(length(tree.pairs), "tree pairs"))
+    
     distances <- lapply(tree.pairs, continuous.distance)
     distances <- as.numeric(unlist(distances))
     distances
+}
+
+
+continuous.distance <- function(two.trees){
+    d <- abs(two.trees[1] - two.trees[2])  
+    d
 }
