7735bce5118df7e63b94d6903cd5fe1f79011d58
RobLanf <rob.lanfear@gmail.com>
2015-05-01 15:07:30 +1000

    change capitalisation of LnL to lnL
    simple really

--- rwty/R/plot.treespace.R
+++ rwty/R/plot.treespace.R
@@ -13,19 +13,20 @@
 
 plot.treespace <- function(points){
 
-    if(!is.null(points$LnL)){
-        p <- ggplot(data=points, aes(x=x,y=y,fill=LnL)) + 
-            geom_path(alpha=0.2, linetype='dashed') + 
-            geom_point(shape=21, size=4, alpha=0.85, colour='white') + 
-            scale_fill_gradient() +
-            geom_point(data=points, aes(x=x, y=y, colour=Generation), size=2)  + 
-            scale_colour_gradient(low='red', high='white')
+    if(!is.null(points$lnL)){
+        p <- ggplot(data=points, aes(x=x,y=y,fill=lnL)) + 
+            geom_path(alpha=0.3, aes(colour=Generation)) + 
+            geom_point(shape=21, size=4, alpha=0.75, colour='white') + 
+            scale_fill_gradient() + 
+            scale_colour_gradient(low='red', high='yellow') + 
+            theme(panel.background = element_blank(), axis.line = element_line(color='grey'))
     }
     else{
         p <- ggplot(data=points, aes(x=x,y=y)) + 
-            geom_path(alpha=0.2, linetype='dashed') + 
-            geom_point(data=points, aes(x=x, y=y, colour=Generation), size=2)  + 
-            scale_colour_gradient(low='red', high='white')
+            geom_path(alpha=0.3, aes(colour=Generation)) + 
+            geom_point(size=4, alpha=0.75) + 
+            scale_colour_gradient(low='red', high='yellow') +
+            theme(panel.background = element_blank(), axis.line = element_line(color='grey'))
     }
     p
 }
