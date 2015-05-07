d875656036a465acb8065dddb09edf77f4b49bb8
RobLanf <rob.lanfear@gmail.com>
2015-05-08 08:55:03 +1000

    function to plot any parameter from the ptable
    should be pretty general. For now I default to 1 column, since folks
    rarely have more than ~4 chains, and it makes more sense to stack plots
    vertically so that the generations line up on the x axis.

---
+++ rwty/R/plot.param.R
@@ -0,0 +1,39 @@
+#' New style plotting of cumulative and slide objects
+#' 
+#' Takes a table from cumulative.freq or slide.freq as input.  
+#' Numclades gives the number of clades to plot, starting from the
+#' top.  Since cumulative.freq and slide.freq both sort by sd these
+#' will by default be the most variable clades.
+#'
+#' @param x An rwty.slide or rwty.cumulative object \code{x}
+#' @param numclades The number of clades to plot.  The clades with the highest sd in clade frequency are plotted first, so numclades = 10 will be the 10 most variable clades in the chain. \code{numclades}
+#'
+#' @return thisplot Returns a ggplot object.
+#'
+#' @keywords cumulative, sliding window, mcmc, phylogenetics, plot
+#'
+#' @export
+#' 
+#' @examples
+#' data(fungus)
+#' slide.data <- slide.freq(run1$trees, burnin=100, window.size=20, gens.per.tree=10000)
+#' cpplot <- plot.cladeprobs(input.table = slide.data$slide.table, numclades=25)
+
+plot.param <- function(chains, burnin = 0, parameter = "lnL"){ 
+
+    ptable = merge.ptables(chains, burnin)
+
+    if(parameter %in% names(ptable)){
+
+        param.plot =  ggplot(ptable, aes_string(x="generation", y=parameter)) + 
+                      geom_line() + 
+                      facet_wrap(~chain, ncol=1) +
+                      ggtitle(parameter)
+
+        return(param.plot)
+
+    }else{
+        stop(sprintf("The variable '%s' is not a column in the table of parameters you have supplied", parameter))
+    }
+
+}
