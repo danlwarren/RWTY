01d871cffc83a17efcbb6edb56d51836ed440002
RobLanf <rob.lanfear@gmail.com>
2015-05-28 17:34:41 +0200

    bug fix in autocorrelation plots

--- rwty/R/makeplot.topological.autocorr.R
+++ rwty/R/makeplot.topological.autocorr.R
@@ -1,13 +1,16 @@
 
-makeplot.topological.autocorr.R <- function(chains, burnin = 0, max.intervals = 100, facet = FALSE){
+makeplot.topological.autocorr <- function(chains, burnin = 0, max.intervals = 100, facet = FALSE){
+
+    chains = check.chains(chains)
 
     dat <- topological.autocorr(chains, burnin, max.intervals)
 
     autocorr.plot = ggplot(data=dat, aes(x=sampling.interval, y=Path.distance)) + 
-            geom_line(alpha=0.2, aes(colour = chain)) + geom_point(size = 2, aes(colour = chain))
+            geom_line(alpha=0.2, aes(colour = chain)) + geom_point(size = 2, aes(colour = chain)) + 
+            xlab("median path distance") + ylab("sampling interval")
 
     if(facet) autocorr.plot = autocorr.plot + facet_wrap(~chain, ncol=1) + theme(legend.position="none")
 
-    return(list('plot'=p, 'distances'=dat, 'random distance' = random.interval))
+    return(autocorr.plot)
     
 }
