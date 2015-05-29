35773028bd4259d4d534c0564c85223d7dbfac85
RobLanf <rob.lanfear@gmail.com>
2015-05-29 10:14:50 +0200

    change name of file for plot function

---
+++ rwty/R/makeplot.autocorr.R
@@ -0,0 +1,42 @@
+#' Make autocorrelation plots of tree topologies from MCMC analyses
+#' 
+#' This function takes a list of rwty.trees objects, and makes an
+#' autocorrelation plot for each chain. Each plot shows the median phylogenetic
+#' distance at a series of sampling intervals. In really well behaved MCMC 
+#' analyses, the median distance will stay constant as the sampling interval 
+#' increases. If there is autocorrelation, the median distance will 
+#' increase as the sampling interval increases, and is expected to level
+#' off when the autocorrelation decreases to zero. The function calculates
+#' path distances, though other distances could also be employed.
+#'
+#' @param chains A list of rwty.trees objects. 
+#' @param burnin The number of trees to eliminate as burnin 
+#' @param max.intervals The maximum number of sampling intervals to use 
+#' @facet TRUE/FALSE to turn facetting of the plot on or off (default)
+#'
+#' @return A ggplot2 plot object, with one line (facetting off) or facet
+#' (facetting on) per rwty.trees object.
+#'
+#' @keywords autocorrelation, path distance
+#'
+#' @export
+#' 
+#' @examples
+#' data(fungus)
+#' makeplot.autocorr(chains = list(run1, run2), burnin = 100)
+
+makeplot.autocorr <- function(chains, burnin = 0, max.intervals = 100, facet = FALSE){
+
+    chains = check.chains(chains)
+
+    dat <- topological.autocorr(chains, burnin, max.intervals)
+
+    autocorr.plot = ggplot(data=dat, aes(x=sampling.interval, y=Path.distance)) + 
+            geom_line(alpha=0.2, aes(colour = chain)) + geom_point(size = 2, aes(colour = chain)) + 
+            xlab("sampling interval") + ylab("median path distance")
+
+    if(facet) autocorr.plot = autocorr.plot + facet_wrap(~chain, ncol=1) + theme(legend.position="none")
+
+    return(autocorr.plot)
+    
+}
