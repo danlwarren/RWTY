0b3030546a30e5ae7db8b1512aa617151147cc06
Dan Warren <dan.l.warren@gmail.com>
2015-06-04 13:29:26 +1000

    Beast log files
    Setting up code to auto-locate beast log files.  The test data I have
    from Beast has some weird naming scheme because it’s a multilocus
    analysis, so I’m not sure how generally workable this is going to be.
    Anyway, it just checks to see if the extension is .t, and if so tries
    for a .p log file, or if the extension is .trees it looks for .log.

--- rwty/R/load.trees.R
+++ rwty/R/load.trees.R
@@ -57,16 +57,27 @@ load.trees <- function(file, type="nexus", gens.per.tree=NA, trim=1, logfile=NA,
     if(!is.na(logfile) && !file.exists(logfile)){
       stop(paste("Logfile not found at", logfile))
     }
+    
     # logfile path has been supplied and file exists
     if(!is.na(logfile) && file.exists(logfile)){
       print(paste("Reading parameter values from", basename(logfile)))
       ptable <- read.table(logfile, skip=skip, header=TRUE)
       ptable <- ptable[seq(from=1, to=length(ptable[,1]), by=trim),]
     }
+    
     # If logfile hasn't been supplied try to find it by searching
     if(is.na(logfile)){
-      if(length(grep(".t$", file, perl=TRUE)) > 0){logfile <- sub(".t$", ".p", file, perl=TRUE)}
-      if(length(grep(".trees$", file, perl=TRUE)) > 0){logfile <- sub(".trees$", ".p", file, perl=TRUE)}
+      
+      # try mrbayes style log file name
+      if(grepl("\\.t$", file)){
+        logfile <- sub("\\.t$", ".p", file)
+      }
+      
+      # try beast style log file name
+      if(grepl("\\.trees$", file)){
+        logfile <- sub("\\.trees$", ".log", file)
+      }
+
       if(file.exists(logfile)){
           print(paste("Reading parameter values from", basename(logfile)))
           ptable <- read.table(logfile, skip=skip, header=TRUE)
