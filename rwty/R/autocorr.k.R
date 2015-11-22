f62a4a30b6efd97a4d2916de8056276fb40fd914
Dan Warren <dan.l.warren@gmail.com>
2015-11-23 10:39:26 +1100

    Created estimate.autocorr.k, revised makeplot.autocorr
    Moved estimate.autocorr.k to its own file, revised make plot.autocorr
    so that it uses that function to get the autocorr.k.  Autocorr.k table
    is still stuffed into the autocorr.plot object.

---
+++ rwty/R/autocorr.k.R
@@ -0,0 +1,41 @@
+#' Calculate sampling interval based on exponential semivariogram model.
+#' 
+#' This function uses an exponential semivariogram model to estimate the asymptotic path distance, and
+#' uses that to estimate the sampling interval at which path distances have reached some fixed
+#' proportion of that value (default 0.95).  It expects as input a data table output by rwty's
+#' 
+#'
+#' @param dat A data frame output from topological.autocorr.
+#' @param ac.cutoff The proportion of the asymptotic path distance to use as a cutoff for determining sampling interval.
+#'
+#' @return A data frame consisting of the k value matching the ac.cutoff proportion of the asymptotic 
+#' path distance for each chain.  This sampling interval estimates the interval at which path distances
+#' are no longer autocorrelated.
+#' 
+#' @keywords autocorrelation, path distance
+#'
+#' @export autocorr.k
+#' @examples
+#' data(fungus)
+#' sampling.table <- topological.autocorr(fungus, burnin = 20, max.intervals = 40)
+#' autocorr.k(sampling.table, ac.cutoff = 0.9)
+
+
+autocorr.k <- function(dat, ac.cutoff = 0.95){
+  
+  # Build an empty data frame
+  autocorr.k <- data.frame(autocorr.time = rep(NA, length(unique(dat$chain))), row.names = unique(dat$chain))
+  for(i in 1:nrow(autocorr.k)){
+    thischain <- rownames(autocorr.k)[i]
+    thisdata <- dat[dat$chain == thischain,]
+    this.ac.result <- optim(par = c(1, 1), 
+                            function(data, par){sum((par[1] * (1 - exp(-(data$sampling.interval/par[2]))) - data$Path.distance)^2)}, 
+                            data = thisdata)
+    if(any(thisdata$Path.distance/this.ac.result$par[1] > ac.cutoff)){
+      autocorr.k[i,1] <- thisdata$sampling.interval[min(which(thisdata$Path.distance/this.ac.result$par[1] > ac.cutoff))]
+    } else {
+      autocorr.k[i,1] <- "> Max"
+    }
+  }
+  return(autocorr.k)
+}
--- rwty/R/makeplot.autocorr.R
+++ rwty/R/makeplot.autocorr.R
@@ -16,7 +16,7 @@
 #' @param facet TRUE/FALSE to turn facetting of the plot on or off (default)
 #'
 #' @return A ggplot2 plot object, with one line (facetting off) or facet
-#' (facetting on) per rwty.trees object, and a data frame containing estimated minimum sampling intervals for each chain.
+#' (facetting on) per rwty.trees object.
 #'
 #' @keywords autocorrelation, path distance
 #'
@@ -37,25 +37,9 @@ makeplot.autocorr <- function(chains, burnin = 0, max.intervals = 100, ac.cutoff
             theme(axis.title.x = element_text(vjust = -.5), axis.title.y = element_text(vjust=1.5))
 
     if(facet) autocorr.plot = autocorr.plot + facet_wrap(~chain, ncol=1) + theme(legend.position="none")
-    
-    #' This bit uses an exponential semivariogram model to estimate the asymptotic path distance, and
-    #' uses that to estimate the sampling interval at which path distances have reached some fixed
-    #' proportion of that value (default 0.95).
-    autocorr.k <- data.frame(autocorr.time = rep(NA, length(unique(dat$chain))), row.names = unique(dat$chain))
-    for(i in 1:nrow(autocorr.k)){
-      thischain <- rownames(autocorr.k)[i]
-      thisdata <- dat[dat$chain == thischain,]
-      this.ac.result <- optim(par = c(1, 1), 
-                              function(data, par){sum((par[1] * (1 - exp(-(data$sampling.interval/par[2]))) - data$Path.distance)^2)}, 
-                              data = thisdata)
-      if(any(thisdata$Path.distance/this.ac.result$par[1] > ac.cutoff)){
-        autocorr.k[i,1] <- thisdata$sampling.interval[min(which(thisdata$Path.distance/this.ac.result$par[1] > ac.cutoff))]
-      } else {
-        autocorr.k[i,1] <- "> Max"
-      }
-    }
-    autocorr.plot$autocorr.k <- autocorr.k
 
+    autocorr.plot$autocorr.k <- estimate.autocorr.k(dat, ac.cutoff)
+    
     autocorr.plot <- list(autocorr.plot = autocorr.plot)
     
     return(autocorr.plot)
