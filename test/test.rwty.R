5e1d59751294bcd31e8d6d394264b2533e22c065
Dan Warren <dan.l.warren@gmail.com>
2015-11-23 16:56:45 +1100

    Fleshing out test code
    Added a section for each plot expected from the test chain.  Not a lot
    of tests for some of them yet other than seeing whether they’re a
    ggplot object, but the framework is there.

--- test/test.rwty.R
+++ test/test.rwty.R
@@ -8,7 +8,7 @@ library(rwty)
 test.trees <- load.trees("~/GitHub/RWTY/test/testchain.t", skip=0)
 
 # Evaluate the trees object
-expect_equal(names(tes.ttrees), c("trees", "ptable", "gens.per.tree"))
+expect_equal(names(test.trees), c("trees", "ptable", "gens.per.tree"))
 expect_equal(length(names(test.trees$trees)), 16)
 expect_equal(names(test.trees$trees), c("tree_0", "tree_100", "tree_200", "tree_300", 
                                         "tree_400", "tree_500", "tree_600", "tree_700", 
@@ -35,3 +35,69 @@ expect_equal(class(LnL), c("gg", "ggplot"))
 expect_equal(names(LnL), c("data", "layers", "scales", "mapping", "theme", 
                            "coordinates", "facet","plot_env", "labels" ))
 expect_equal(LnL$data$Gen, seq(0, 1500, by=100))
+expect_equal(LnL$data$LnL, c(60, 50, 50, 50, 50, 50, 60, 50, 60, 50, 50, 50, 50, 50, 60, 50))
+
+
+# Test attributes of Param1
+Param1 <- test.output$Param1
+expect_equal(class(Param1), c("gg", "ggplot"))
+expect_equal(names(Param1), c("data", "layers", "scales", "mapping", "theme", 
+                           "coordinates", "facet","plot_env", "labels" ))
+expect_equal(Param1$data$Gen, seq(0, 1500, by=100))
+expect_equal(Param1$data$Param1, c(8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7))
+
+
+# Test attributes of Param2
+Param2 <- test.output$Param2
+expect_equal(class(Param2), c("gg", "ggplot"))
+expect_equal(names(Param2), c("data", "layers", "scales", "mapping", "theme", 
+                              "coordinates", "facet","plot_env", "labels" ))
+expect_equal(Param2$data$Gen, seq(0, 1500, by=100))
+expect_equal(Param2$data$Param2, rep(5, 16))
+
+
+# Test attributes of heatmap
+heatmap <- test.output$heatmap
+expect_equal(names(heatmap), c("data", "layers", "scales", "mapping", "theme", 
+                              "coordinates", "facet","plot_env", "labels" ))
+expect_equal(heatmap$data$sample, c(1,5,9,13))
+
+
+# Test attributes of points.plot
+points.plot <- test.output$points.plot
+expect_equal(names(points.plot), c("data", "layers", "scales", "mapping", "theme", 
+                               "coordinates", "facet","plot_env", "labels" ))
+expect_equal(heatmap$data$sample, c(1,5,9,13))
+
+
+# Test attributes of slide posterior plot
+slide.posterior.plot <- test.output[["Chain.1 Sliding Window Posterior Probability"]]
+expect_equal(names(slide.posterior.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                   "coordinates", "facet","plot_env", "labels" ))
+
+# Test attributes of cumulative posterior plot
+cumulative.posterior.plot <- test.output[["Chain.1 Cumulative Posterior Probability"]]
+expect_equal(names(cumulative.posterior.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                            "coordinates", "facet","plot_env", "labels" ))
+
+
+# Test attributes of slide variance plot
+slide.variance.plot <- test.output[["Chain.1 Sliding Window Variance"]]
+expect_equal(names(slide.variance.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                            "coordinates", "facet","plot_env", "labels" ))
+
+# Test attributes of cumulative variance plot
+cumulative.variance.plot <- test.output[["Chain.1 Cumulative Variance"]]
+expect_equal(names(cumulative.variance.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                                 "coordinates", "facet","plot_env", "labels" ))
+
+# Test attributes of ess plot
+ess.plot <- test.output$ess.plot
+expect_equal(names(ess.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                                "coordinates", "facet","plot_env", "labels" ))
+
+
+# Test attributes of autocorr plot
+autocorr.plot <- test.output$autocorr.plot
+expect_equal(names(autocorr.plot), c("data", "layers", "scales", "mapping", "theme", 
+                                                "coordinates", "facet","plot_env", "labels", "autocorr.k" ))
