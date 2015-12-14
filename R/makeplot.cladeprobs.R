d8aff946d89ec26fadc92e5cac840533c27f4cb1
RobLanf <rob.lanfear@gmail.com>
2015-12-14 12:27:34 +1100

    new makeplot.cladeprobs plots
    These now have the same interface as the other makeplot functions, and
    you can turn faceting on or off. The latter gives you a list of plots
    by chain name.
    
    As for other functions, you now call this function by passing in chains
    themselves.

--- R/makeplot.cladeprobs.R
+++ R/makeplot.cladeprobs.R
@@ -15,26 +15,54 @@
 #' @export makeplot.cladeprobs
 #' @examples
 #' data(fungus)
-#' slide.data <- slide.freq(fungus$Fungus.Run1$trees, burnin=20, window.size=20, gens.per.tree=10000)
-#' makeplot.cladeprobs(input.table = slide.data$slide.table, numclades=25)
+#' makeplot.cladeprobs(fungus, burnin = 20, numclades=25)
 
-makeplot.cladeprobs <- function(input.table, numclades=20){ 
-    # clade probability plot over generations
+makeplot.cladeprobs <- function(chains, burnin = 0, n.clades=20, window.size = 20, facet = TRUE){ 
+
+
+    chains = check.chains(chains)
+    slide.freq.list = slide.freq(chains, burnin = burnin, window.size = window.size)
+    dat.list = lapply(slide.freq.list, process.slide.freq.table, n.clades = n.clades)
+    dat = do.call("rbind", dat.list)
+    dat$Chain = rep(names(dat.list), each = nrow(dat.list[[1]]))
+    rownames(dat) = NULL
     
-    # TODO should put a check in here to make sure it only accepts slidetest or cumtest objects
-    
-    x <- input.table[1:numclades,2:length(input.table) - 2] #Stripping off mean and SD
-    x$clade <- rownames(x)
-    x <- melt(x, id.vars="clade")
-    colnames(x) <- c("Clade", "Generations", "Posterior.Probability")
-    #print(x$Generations)
-    x$Clade <- as.factor(x$Clade)
-    
-    #print(x)
-    thisplot <- ggplot(data=x, aes(x=as.numeric(as.character(Generations)), y=Posterior.Probability, group=Clade, color=Clade)) + 
+    if(facet==TRUE){
+        cladeprob.plot <- ggplot(data=dat, aes(x=as.numeric(as.character(Generations)), y=Posterior.Probability, group = Clade, color = StDev)) + 
+            facet_wrap(~Chain, ncol = 1) +
+            geom_line() +
+            xlab("Generations")    
+    }else{
+        dat.list = split(dat, f = dat$Chain)
+        cladeprob.plot = lapply(dat.list, single.plot)
+    }
+
+    return(cladeprob.plot)
+}
+
+
+single.plot <- function(dat){
+
+    cladeprob.plot <- ggplot(data=dat, aes(x=as.numeric(as.character(Generations)), y=Posterior.Probability, group = Clade, color = StDev)) + 
         geom_line() +
-        theme(legend.position="none") +
-        xlab("Generations") +
-        theme(axis.title.x = element_text(vjust = -.5), axis.title.y = element_text(vjust=1.5))
-    thisplot
+        xlab("Generations")    
+
+    return(cladeprob.plot)
+
+}
+
+
+process.slide.freq.table <- function(slide.freq.table, n.clades){
+
+    # strip out just the parts of a slide.freq.table that we need
+
+    dat = slide.freq.table$slide.table
+    dat = dat[1:n.clades,2:length(dat) - 1] #Stripping off mean
+    dat$clade = rownames(dat)
+    dat = melt(dat, id.vars=c("clade", "sd"))
+    colnames(dat) = c("Clade", "StDev", "Generations", "Posterior.Probability")
+    dat$Clade = as.factor(dat$Clade)
+    dat$id = rep(1:length(unique(dat$Clade)), length.out = nrow(dat))
+
+    return(dat)
 }
--- R/slide.freq.R
+++ R/slide.freq.R
@@ -17,84 +17,74 @@
 #' @export slide.freq
 #' @examples
 #' data(fungus)
-#' slide.data <- slide.freq(fungus$Fungus.Run1$trees, burnin=20, window.size=20, gens.per.tree=10000)
+#' slide.data <- slide.freq(fungus, burnin=20)
 
-slide.freq <- function(tree.list, burnin=0, window.size, gens.per.tree = 1){ 
-    #Specify burnin in TREES, not GENERATIONS
-    
-    # Peel just the trees off of rwty.trees object
-    # so the function can take rwty.trees or multiPhylo
-    if(class(tree.list) == "rwty.trees"){tree.list <- tree.list$trees}
-    
-    print(paste("input list length:", length(tree.list)))
-    print(paste("burnin:", burnin))
+slide.freq <- function(chains, burnin=0, window.size = 50){ 
 
-    tree.list <- tree.list[(burnin+1):length(tree.list)]
+    chains = check.chains(chains)
+    trees = lapply(chains, function(x) x[['trees']])
+    slide.freq.list = lapply(trees, get.slide.freq.table, burnin = burnin, window.size = window.size, gens.per.tree = chains[[1]]$gens.per.tree)
+    return(slide.freq.list)
 
-    N <- length(tree.list)
+}
 
-    print(paste("post-burnin list length:", length(tree.list)))
 
-    n.windows <- as.integer(N/window.size)
 
-    print(paste("number of windows:", n.windows))
 
-    #print("a")
+get.slide.freq.table <- function(tree.list, burnin = 0, window.size = 50, gens.per.tree = 1){
+
+    # take a list of trees and return a table of posterior probabilities of clades
+    # from a sliding window
+    n.windows = as.integer(length(tree.list)/window.size)
 
     # first we slice up our tree list into smaller lists
-    tree.index <- seq_along(tree.list)
-    tree.windows <- split(tree.list, ceiling(tree.index/window.size))[1:n.windows]
-
-    #print("b")
-    
-    
+    tree.index = seq_along(tree.list)
+    tree.windows = split(tree.list, ceiling(tree.index/window.size))[1:n.windows]
 
     # now we calculate clade frequencies on each of the lists of trees
-    clade.freq.list <- lapply(tree.windows, clade.freq, start=1, end=window.size)
+    clade.freq.list = lapply(tree.windows, clade.freq, start=1, end=window.size)
     
     # and rename the list to the first tree of each window
-    names(clade.freq.list) <- prettyNum(seq((burnin + 1),(burnin + length(clade.freq.list) * window.size), by=window.size)*gens.per.tree, sci=TRUE)
+    names(clade.freq.list) = prettyNum(seq((burnin + 1),(burnin + length(clade.freq.list) * window.size), by=window.size)*gens.per.tree, sci=TRUE)
 
     # this is the table of frequencies in each window
-    #print("Working on window 1")
-    slide.freq.table <- clade.freq.list[[1]]
-    colnames(slide.freq.table)[-1] <- names(clade.freq.list)[1]
+    slide.freq.table = clade.freq.list[[1]]
+    colnames(slide.freq.table)[-1] = names(clade.freq.list)[1]
 
     # now add in the rest
     for(i in 2:length(clade.freq.list)){
-        # print(paste("Working on window", i))
-        # add data to slide.freq table
-        slide.freq.table <- merge(slide.freq.table, clade.freq.list[[i]], by="cladenames", all=TRUE)
-        colnames(slide.freq.table)[which(colnames(slide.freq.table)=="cladefreqs")] <- names(clade.freq.list)[i]
+        slide.freq.table = merge(slide.freq.table, clade.freq.list[[i]], by="cladenames", all=TRUE)
+        colnames(slide.freq.table)[which(colnames(slide.freq.table)=="cladefreqs")] = names(clade.freq.list)[i]
     }
 
-    slide.freq.table[is.na(slide.freq.table)] <- 0.0
-    rownames(slide.freq.table) <- slide.freq.table$cladenames
-    slide.freq.table <- slide.freq.table[,-1]
+    # tidy things up
+    slide.freq.table[is.na(slide.freq.table)] = 0.0
+    rownames(slide.freq.table) = slide.freq.table$cladenames
+    slide.freq.table = slide.freq.table[,-1]
     
     # calculate sd and mean of cumulative frequency and mean
-    thissd <- apply(slide.freq.table, 1, sd)
+    thissd = apply(slide.freq.table, 1, sd)
     
-    thismean <- apply(slide.freq.table, 1, mean) 
-    slide.freq.table$sd <- thissd
-    slide.freq.table$mean <- thismean
+    thismean = apply(slide.freq.table, 1, mean) 
+    slide.freq.table$sd = thissd
+    slide.freq.table$mean = thismean
     
     # Sorting by sd, since these are usually the most interesting clades
-    slide.freq.table <- slide.freq.table[order(slide.freq.table$sd, decreasing=TRUE),]
+    slide.freq.table = slide.freq.table[order(slide.freq.table$sd, decreasing=TRUE),]
     
     # Building a new table that contains parsed clade names
-    translation.table <- cbind(as.numeric(as.factor(rownames(slide.freq.table))), 
+    translation.table = cbind(as.numeric(as.factor(rownames(slide.freq.table))), 
                                 as.character(rownames(slide.freq.table)), 
                                 parse.clades(rownames(slide.freq.table), tree.list))
-    colnames(translation.table) <- c("Clade number", "Tip numbers", "Tip names")
+    colnames(translation.table) = c("Clade number", "Tip numbers", "Tip names")
     
     # Seting slide.freq.table to the same names as the translation table
-    rownames(slide.freq.table) <- as.numeric(as.factor(rownames(slide.freq.table)))
+    rownames(slide.freq.table) = as.numeric(as.factor(rownames(slide.freq.table)))
     
     output <- list("slide.table" = slide.freq.table, "translation" = translation.table)
     class(output) <- "rwty.slide"
-    output
+    
+    return(output)
 }
-
 
    
