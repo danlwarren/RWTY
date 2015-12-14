8a56dded906d1cfde6d0f0590c39ec08b7488dde
RobLanf <rob.lanfear@gmail.com>
2015-12-14 15:30:55 +1100

    add function to make cumulative frequency plots

---
+++ R/makeplot.cladeprobs.cumulative.R
@@ -0,0 +1,45 @@
+#' Plot cumulative clade probabilities over the course of an MCMC
+#' 
+#' Takes a list of rwty.trees objects.  
+#' Plots the cumulative posterior probabilities of clades over the course of the MCMC. Stationarity is indicated by clade probabilities levelling out.
+#' Only plots the n.clades most variable clades, as measured by the standard deviation of the posterior probabilities of each clade across all windows.  
+#' Each line in the plot represents a single clade. The colour of the line represents the standard deviation of the posterior probabilities of that clade across all sliding windows.
+#'
+#' @param chains A list of rwty.trees objects. 
+#' @param burnin The number of trees to eliminate as burnin 
+#' @param n.clades The number of clades to plot 
+#' @param window.size The number of trees to include in each window (note, specified as a number of sampled trees, not a number of generations)
+#' @param facet (TRUE/FALSE). TRUE: return a single plot with one facet per chain; FALSE: return a list of individual plots with one plot per chain 
+#'
+#' @return cladeprobs.plot Either a single ggplot2 object or a list of ggplot2 objects.
+#'
+#' @keywords sliding window, mcmc, phylogenetics, plot
+#'
+#' @export makeplot.cladeprobs.cumulative
+#' @examples
+#' data(fungus)
+#' makeplot.cladeprobs.cumulative(fungus, burnin = 20, n.clades=25)
+
+makeplot.cladeprobs.cumulative <- function(chains, burnin = 0, n.clades=20, window.size = 20, facet = TRUE){ 
+
+
+    chains = check.chains(chains)
+    cumulative.freq.list = cumulative.freq(chains, burnin = burnin, window.size = window.size)
+    dat.list = lapply(cumulative.freq.list, process.freq.table, n.clades = n.clades)
+    dat = do.call("rbind", dat.list)
+    dat$Chain = rep(names(dat.list), each = nrow(dat.list[[1]]))
+    rownames(dat) = NULL
+    
+    if(facet==TRUE){
+        cladeprobs.plot <- ggplot(data=dat, aes(x=as.numeric(as.character(Generations)), y=Posterior.Probability, group = Clade, color = StDev)) +
+            facet_wrap(~Chain, ncol = 1) +
+            geom_line() +
+            xlab("Generations")
+    }else{
+        dat.list = split(dat, f = dat$Chain)
+        cladeprobs.plot = lapply(dat.list, single.cladeprob.plot)
+    }
+
+    return(cladeprobs.plot)
+}
+
