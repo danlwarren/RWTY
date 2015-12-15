2aae40fab1c369f237b2a3b7a3cff6cdc5d0c690
RobLanf <rob.lanfear@gmail.com>
2015-12-15 12:44:37 +1100

    rename makeplot.ess to makeplot.pseudo.ess

--- R/makeplot.pseudo.ess.R
+++ R/makeplot.pseudo.ess.R
@@ -1,5 +1,7 @@
 #' Plot the pseudo ESS of tree topologies from MCMC chains.
 #'
+#' NB: We do not recommend the use of this ESS value. For calculating the ESS you should use
+#' the approximate ESS, see the topological.approx.ess() function in rwty.
 #' This function takes a list of rwty.trees objects, and plots the
 #' pseudo ESS of the trees from each chain, after removing burnin. 
 #' Each caulcation is repeated n times, where in each replicate a random
@@ -14,19 +16,18 @@
 #' @param burnin The number of trees to eliminate as burnin 
 #' @param n The number of replicate analyses to do 
 #'
-#' @return A ggplot2 plot object, in which each chain is represented by a point
+#' @return pseudo.ess.plot A ggplot2 plot object, in which each chain is represented by a point
 #' which represents the median pseudo ESS from the n replicates, and
 #' whiskers representing the upper and lower 95% intervals of the n replicates. 
 #'
 #' @keywords ESS, path distance
 #'
-#' @export makeplot.ess
+#' @export makeplot.pseudo.ess
 #' @examples
 #' data(fungus)
-#' makeplot.ess(fungus, burnin = 20, n = 10)
-#' # demostrates severe issues with run1!
+#' makeplot.pseudo.ess(fungus, burnin = 20, n = 10)
 
-makeplot.ess <- function(chains, burnin = 0, n = 50){
+makeplot.pseudo.ess <- function(chains, burnin = 0, n = 50){
 
     chains <- check.chains(chains)
 
@@ -37,7 +38,7 @@ makeplot.ess <- function(chains, burnin = 0, n = 50){
                       ci.upper = apply(dat, 2, FUN = function(x) quantile(x, .975)),
                       chain = names(chains))
     
-    ess.plot <- ggplot(dat, aes(x=chain, y=median.ess, colour = chain)) + 
+    pseudo.ess.plot <- ggplot(dat, aes(x=chain, y=median.ess, colour = chain)) + 
       geom_errorbar(aes(ymin=ci.lower, ymax=ci.upper), size=1, width=.3) +
       geom_point(size=3) +
       xlab("Chain") +
@@ -45,10 +46,6 @@ makeplot.ess <- function(chains, burnin = 0, n = 50){
       theme(axis.title.x = element_text(vjust = -.5), axis.title.y = element_text(vjust=1.5)) +
       expand_limits(y=0)
     
-    # for some reason these plots need to be returned as lists, or analyze.simple flattens them and puts
-    # the plot components, rather than the plot itself, into the final object
-    ess.plot <- list(ess.plot = ess.plot)
-
-    return(ess.plot)
+    return(pseudo.ess.plot)
     
 }
