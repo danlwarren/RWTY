de7e28b83e977d787fd4765e0375408b6ccb0703
RobLanf <rob.lanfear@gmail.com>
2016-03-08 20:57:41 +1100

    use the mean not the median
    it works better as a measure of central tendency in this instance

--- R/makeplot.autocorr.R
+++ R/makeplot.autocorr.R
@@ -1,10 +1,10 @@
 #' Make autocorrelation plots of tree topologies from MCMC analyses
 #' 
 #' This function takes a list of rwty.trees objects, and makes an
-#' autocorrelation plot for each chain. Each plot shows the median phylogenetic
+#' autocorrelation plot for each chain. Each plot shows the mean phylogenetic
 #' distance at a series of sampling intervals. In really well behaved MCMC 
-#' analyses, the median distance will stay constant as the sampling interval 
-#' increases. If there is autocorrelation, the median distance will 
+#' analyses, the mean distance will stay constant as the sampling interval 
+#' increases. If there is autocorrelation, the mean distance will 
 #' increase as the sampling interval increases, and is expected to level
 #' off when the autocorrelation decreases to zero. The function calculates
 #' path distances, though other distances could also be employed.
@@ -44,9 +44,9 @@ makeplot.autocorr <- function(chains, burnin = 0, autocorr.intervals = 100, squa
     }
 
     if(squared == TRUE){
-        y.label = sprintf("Median Squared %s between Pairs of Trees", td.name)
+        y.label = sprintf("Mean Squared %s between Pairs of Trees", td.name)
     }else{
-        y.label = sprintf("Median %s between Pairs of Trees", td.name)
+        y.label = sprintf("Mean %s between Pairs of Trees", td.name)
     }
 
     autocorr.plot = ggplot(data=dat, aes(x=sampling.interval, y=topo.distance)) + 
