bda7d42fb57d704b98ead2fe7f16b6c71363a26d
Luke Harmon <lukeharmon@Lukes-MacBook-Pro.local>
2016-08-15 11:13:37 -0700

    add revbayes tree loading

--- R/load.multi.R
+++ R/load.multi.R
@@ -1,12 +1,13 @@
 #' Load all matching files from a directory into a list of rwty.trees objects
-#' 
+#'
 #' Finds trees and log files based on format definition, returns rwty.trees objects containing both
 #'
 #' @param path The path to the directory containing tree and log files
-#' @param format File format, which is used to find tree and log files.  
+#' @param format File format, which is used to find tree and log files.
 #' Currently accepted values are "mb" for MrBayes, "beast" for BEAST, and "*beast" for *BEAST.
+#' LJH: Trying to add support for format="revbayes"
 #' If you would like RWTY to understand additional formats, please contact the authors and send us some sample data.
-#' @param labels A vector of names to assign to chains as they are read in.  
+#' @param labels A vector of names to assign to chains as they are read in.
 #' @param ... Further arguments to be passed to load.trees.
 #' @return output A list of rwty.trees objects containing the multiPhylos and the tables of values from the log files if available.
 #'
@@ -17,23 +18,23 @@
 #' #load.multi(path = "~/my trees/", format = "*beast")
 
 load.multi <- function(path = ".", format = "mb", labels=NA, ...){
-  
+
   file.format <- get.format(format)
-  
+
   output <- list()
-  
+
   # Convert arguments to regex
   ext.tree <- paste0("\\",file.format$trees.suffix, "$")
   ext.p <- file.format$log.suffix
-  
+
   # Find t and p files
   tfiles <- list.files(path, pattern=ext.tree, full.names=TRUE)
   pfiles <- unlist(lapply(tfiles, FUN = function(x) sub(ext.tree, ext.p, x)))
-  
+
   if(length(tfiles) == 0){
     stop("Couldn't find any tree files")
   }
-  
+
   # Step through tfiles, find log files if available, load chains
   for(i in 1:length(tfiles)){
     print(basename(tfiles[i]))
@@ -44,14 +45,14 @@ load.multi <- function(path = ".", format = "mb", labels=NA, ...){
       output[[i]] <- load.trees(tfiles[i], format = format, ...)
     }
   }
-  
+
   # Add names to chains
   if(is.na(labels)){
-    names(output) <- lapply(tfiles, FUN = function(x) basename (x))  
+    names(output) <- lapply(tfiles, FUN = function(x) basename (x))
   }
   else{
     names(output) <- labels
   }
-  
+
   output
 }
--- R/load.trees.R
+++ R/load.trees.R
@@ -1,12 +1,12 @@
 #' Custom functions to load tree lists so that rwty can do basic processing on the way in.
-#' 
+#'
 #' Loads trees, looks for a log file of tree likelihoods and parameter values, returns an rwty.trees object containing both
 #'
 #' @param file A path to a tree file containing an MCMC chain of trees
 #' @param type An argument that designates the type of tree file.  If "nexus",
 #' trees are loaded using ape's read.nexus function.  Otherwise, it's read.tree.
 #' If a "format" argument is passed, type will be determined from the format definition.
-#' @param format File format, which is used to find tree and log files.  
+#' @param format File format, which is used to find tree and log files.
 #' Currently accepted values are "mb" for MrBayes, "beast" for BEAST, and "*beast" for *BEAST.
 #' If you would like RWTY to understand additional formats, please contact the authors and send us some sample data.
 #' @param gens.per.tree The number of generations separating trees.  If not provided, RWTY will attempt to calculate it automatically.
@@ -14,9 +14,9 @@
 #' @param logfile A path to a file containing model parameters and likelihoods.  If no path is provided but
 #' a "format" argument is supplied, RWTY will attempt to find the log file automatically based on the format
 #' definition.
-#' @param skip The number of lines that must be skipped to get to the header of the log file.  
+#' @param skip The number of lines that must be skipped to get to the header of the log file.
 #' MrBayes, for instance, prints a comment line at the top of the log file, so MrBayes files should be
-#' read in with a skip value of 1.  If no "skip" value is provided but a "format" is supplied, RWTY will 
+#' read in with a skip value of 1.  If no "skip" value is provided but a "format" is supplied, RWTY will
 #' attempt to read logs using the skip value from the format definition.
 #' @return output An rwty.trees object containing the multiPhylo and the table of values from the log file if available.
 #'
@@ -27,31 +27,42 @@
 #' #load.trees(file="mytrees.t", format = "mb")
 
 load.trees <- function(file, type=NA, format = "mb", gens.per.tree=NA, trim=1, logfile=NA, skip=NA){
-  
+
   file.format <- get.format(format)
-  
+
   if(is.na(type)){
-    type <- file.format$type 
+    type <- file.format$type
   }
-  
+
   if(is.na(skip)){
     skip <- file.format$skip
   }
 
   # Read in trees
   print("Reading trees...")
-  if(type == "nexus") {treelist <- read.nexus(file=file)}
-  else {treelist <- read.tree(file=file)}
-  
-  treelist <- treelist[seq(from=1, to=length(treelist), by=trim)]
-  
-  if(is.na(gens.per.tree)){
-    gens.per.tree <- as.numeric(tail(strsplit(x=names(treelist)[3], split="[[:punct:]]")[[1]], 1)) - 
-      as.numeric(tail(strsplit(x=names(treelist)[2], split="[[:punct:]]")[[1]], 1))  
+  if(type == "nexus") {
+    treelist <- read.nexus(file=file)
+  } else if(type=="revbayes") {
+    tmp <- read.revbayestrees(file=file)
+    treelist <- tmp$tree
+    ptable <- tmp$param
+  } else {
+    treelist <- read.tree(file=file)
   }
   
+  treelist <- treelist[seq(from=1, to=length(treelist), by=trim)]
+
+  if(is.na(gens.per.tree)){
+    if(type=="revbayes") {
+      gens.per.tree <- rbparam[2,"Iteration"] - rbparam[1,"Iteration"]
+    } else {
+      gens.per.tree <- as.numeric(tail(strsplit(x=names(treelist)[3], split="[[:punct:]]")[[1]], 1)) -
+        as.numeric(tail(strsplit(x=names(treelist)[2], split="[[:punct:]]")[[1]], 1))
+    }
+  }
+
   print(paste(gens.per.tree, "generations per tree..."))
-  
+
   # Unroot all trees.  Can't use lapply because it was
   # deleting tip labels.
   if(is.rooted(treelist[[1]])){
@@ -61,54 +72,57 @@ load.trees <- function(file, type=NA, format = "mb", gens.per.tree=NA, trim=1, l
     }
   }
   else{print("Trees are unrooted...")}
-  
-  
+
+
   # Reset class
   class(treelist) <- "multiPhylo"
   
-  ptable <- NULL
-  
-  # Check whether log file path has been supplied and doesn't exist
-  if(!is.na(logfile) && !file.exists(logfile)){
-    stop(paste("Logfile not found at", logfile))
-  }
-  
-  # logfile path has been supplied and file exists
-  if(!is.na(logfile) && file.exists(logfile)){
-    print(paste("Reading parameter values from", basename(logfile)))
-    ptable <- read.table(logfile, skip=skip, header=TRUE)
+  if(format=="revbayes") {
     ptable <- ptable[seq(from=1, to=length(ptable[,1]), by=trim),]
-  }
+  } else {
+    ptable <- NULL
+
+    # Check whether log file path has been supplied and doesn't exist
+    if(!is.na(logfile) && !file.exists(logfile)){
+      stop(paste("Logfile not found at", logfile))
+    }
   
-  # If logfile hasn't been supplied try to find it by searching
-  if(is.na(logfile)){
-    
-    logfile <- sub(pattern = paste0(file.format$trees.suffix, "$"), file.format$log.suffix, file)
-    
+    # logfile path has been supplied and file exists
     if(!is.na(logfile) && file.exists(logfile)){
       print(paste("Reading parameter values from", basename(logfile)))
       ptable <- read.table(logfile, skip=skip, header=TRUE)
       ptable <- ptable[seq(from=1, to=length(ptable[,1]), by=trim),]
-    } else {
-      print(paste("Couldn't find", basename(logfile)))
+    }
+  
+    # If logfile hasn't been supplied try to find it by searching
+    if(is.na(logfile)){
+  
+      logfile <- sub(pattern = paste0(file.format$trees.suffix, "$"), file.format$log.suffix, file)
+  
+      if(!is.na(logfile) && file.exists(logfile)){
+        print(paste("Reading parameter values from", basename(logfile)))
+        ptable <- read.table(logfile, skip=skip, header=TRUE)
+        ptable <- ptable[seq(from=1, to=length(ptable[,1]), by=trim),]
+      } else {
+        print(paste("Couldn't find", basename(logfile)))
+      }
+  
     }
 
-  }
-  
-  
+
   output <- list(
-    "trees" = treelist, 
-    "ptable" = ptable, 
+    "trees" = treelist,
+    "ptable" = ptable,
     "gens.per.tree" = gens.per.tree)
-  
+
   class(output) <- "rwty.trees"
-  
+
   output
 }
 
 # This function takes the name of a format and returns a list containing important info about file suffixes and whatnot
 get.format <- function(format){
-  
+
   # Default behavior for MrBayes files
   if(format == "mb"){
     return(list(
@@ -118,7 +132,7 @@ get.format <- function(format){
       skip = 1
     ))
   }
-  
+
   # Default behavior for *BEAST files
   if(format == "*beast"){
     return(list(
@@ -128,7 +142,7 @@ get.format <- function(format){
       skip = 2
     ))
   }
-  
+
   # Default behavior for BEAST files
   if(format == "beast"){
     return(list(
@@ -138,5 +152,15 @@ get.format <- function(format){
       skip = 2
     ))
   }
-  
+
+  # Default behavior for revbayes files
+  if(format == "revbayes"){
+    return(list(
+      trees.suffix = ".trees",
+      log.suffix = ".log",
+      type = "revbayes",
+      skip = 1
+    ))
+  }
+
 }
