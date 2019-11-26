140723fa89365280bb14dde52be6f3decc6113a2
Dan Warren <dan.l.warren@gmail.com>
2019-11-26 17:15:25 +1100

    Fixed codoc mismatch

--- R/analyze.rwty.R
+++ R/analyze.rwty.R
@@ -75,7 +75,7 @@ utils::globalVariables(c("Generation", "CSF", "split.frequency", "ess"))
 #' p
 #' }
 
-analyze.rwty <- function(chains, burnin=0, window.size=20, treespace.points = 100, n.clades = 20,
+analyze.rwty <- function(chains, burnin=NA, window.size=20, treespace.points = 100, n.clades = 20,
                          min.freq = 0.0, fill.color = NA, pdf.filename = NA, 
                          overwrite=FALSE, facet=TRUE, free_y=FALSE, autocorr.intervals=100, ess.reps = 20,
                          treedist = 'PD', params = NA, max.sampling.interval = NA, report.filename = NA, ...){
